fc96b1e34acd6ea24f9ed42109e1839589f65829
Almir <allo0001@stud.kea.dk>
2026-02-06 13:54:28 +0100

    peter med stort P

--- 3caPitalization/index.js
+++ 3caPitalization/index.js
@@ -1,8 +1,11 @@
 const peter = "peter";
 
-const result =
-  peter.substring(0, 2).toLowerCase() +
-  peter.substring(2, 3).toUpperCase() +
-  peter.substring(3).toLowerCase();
-
+const result = peter.substring(0, 1).toUpperCase() + peter.substring(1);
 console.log(result);
+
+// const result =
+//   peter.substring(0, 2).toLowerCase() +
+//   peter.substring(2, 3).toUpperCase() +
+//   peter.substring(3).toLowerCase();
+
+// console.log(result);
