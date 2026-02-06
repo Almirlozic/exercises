f1b80d4d7ab8527d6e3da3951d75ae3962d7147c
Almir <allo0001@stud.kea.dk>
2026-02-06 13:27:33 +0100

    indexOf og substring

---
+++ name-parts/index.js
@@ -0,0 +1,10 @@
+const name = "Peter Heronimous Lind";
+const firstSpace = name.indexOf(" ");
+const lastSpace = name.lastIndexOf(" ");
+const firstName = name.substring(0, firstSpace);
+const middleName = name.substring(firstSpace + 1, lastSpace);
+const lastName = name.substring(lastSpace + 1);
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
