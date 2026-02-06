2705a675c9fb284e498528cfd65d50d3c052c169
Almir <allo0001@stud.kea.dk>
2026-02-06 13:47:43 +0100

    peter med stort T

---
+++ 3caPitalization/index.js
@@ -0,0 +1,8 @@
+const peter = "peter";
+
+const result =
+  peter.substring(0, 2).toLowerCase() +
+  peter.substring(2, 3).toUpperCase() +
+  peter.substring(3).toLowerCase();
+
+console.log(result);
