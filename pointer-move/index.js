c5c4099ad814f23cad04e80ad2cbbe8fd20badd8
Almir <allo0001@stud.kea.dk>
2026-02-11 21:04:30 +0100

    færdig

--- pointer-move/index.js
+++ pointer-move/index.js
@@ -1,5 +1,6 @@
 window.addEventListener("mousemove", function (event) {
-  console.log(event);
+  //console.log(event);
   const percent = (event.clientX / window.innerWidth) * 100;
-  document.documentElement.style.setProperty("--mouse-bg", percent + "%");
+  document.documentElement.style.setProperty("--l-bg", percent + "%");
+  document.documentElement.style.setProperty("--l-color", 100 - percent + "%");
 });
