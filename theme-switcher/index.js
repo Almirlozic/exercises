44de966d85f6f268cbc2f8b081055724db85ae8b
Almir <allo0001@stud.kea.dk>
2026-02-13 14:28:22 +0100

    localstorage

--- theme-switcher/index.js
+++ theme-switcher/index.js
@@ -1,9 +1,17 @@
 const themeSelect = document.querySelector("#switcher");
 const body = document.body;
 
+document.addEventListener("DOMContentLoaded", oldTheme);
+
 themeSelect.value = body.dataset.theme;
 
 themeSelect.addEventListener("change", (event) => {
   const chosenTheme = event.target.value;
   body.dataset.theme = chosenTheme;
+  localStorage.setItem("theme", event.target.value);
 });
+
+function oldTheme() {
+  body.dataset.theme = localStorage.getItem("theme");
+  themeSelect.value = localStorage.getItem("theme");
+}
